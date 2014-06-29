7a8376fb8137f4417ca1863a3acbd458df351a10
Евгений <pilot_m31@rambler.ru>
2014-06-29 18:57:24 +0300

    Jora Home

---
+++ src/com/javarush/test/level07/lesson04/task04/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level07.lesson04.task04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Массив из чисел в обратном порядке
+1. Создать массив на 10 чисел.
+2. Ввести с клавиатуры 10 чисел и записать их в массив.
+3. Расположить элементы массива в обратном порядке.
+4. Вывести результат на экран, каждое значение выводить с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] integers = new int[10];
+        int[] shmintegers = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            integers[i] = Integer.parseInt(reader.readLine());
+            shmintegers[9-i] = integers[i];
+        }
+
+        integers = shmintegers;
+        for (int i=0; i<10; i++) System.out.println(integers[i]);
+    }
+}
